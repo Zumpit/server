9b5eb891e7e2c30aa76a19a88974e248316b088e
imadhrit <maharajrabel@gmail.com>
2021-10-27 20:10:19 +0530

    try binding

--- models/model.go
+++ models/model.go
@@ -43,9 +43,9 @@ type EmailValidation struct {
 }
 
 type EmailQuery struct {
-	Firstname    *string      `json:"firstname"`
-	Lastname     *string      `json:"lastname"`
-	Domain       *string      `json:"domain"`    
+	Firstname    *string      `json:"firstname" binding:"required" `
+	Lastname     *string      `json:"lastname" binding:"required" `
+	Domain       *string      `json:"domain" binding:"required" `    
 }
 
 
